64b3d92f493d7af687523794052e1107f41e79f8
reoky <reoky1@gmail.com>
2014-11-16 15:56:08 -0600

    Challenge Four + Three Fixed

--- challenge-four/app/src/main/java/com/reoky/crackme/challengefour/listeners/ChallengeFourFragmentOnClickListener.java
+++ challenge-four/app/src/main/java/com/reoky/crackme/challengefour/listeners/ChallengeFourFragmentOnClickListener.java
@@ -1,73 +1,30 @@
 package com.reoky.crackme.challengefour.listeners;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.os.Vibrator;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.reoky.crackme.challengefour.R;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import com.reoky.crackme.challengefour.fragments.ChallengeFourFragment;
 
 /**
  * Simple listener class to listen for events on ChallengeFourFragment
  * author: Lucas Thoresen
  */
 public class ChallengeFourFragmentOnClickListener implements View.OnClickListener {
+
+    private ChallengeFourFragment fragment;
+
+    // Since we're not using an anonymous OnClickListener, but rather a named outer OnClickListener
+    public ChallengeFourFragmentOnClickListener(ChallengeFourFragment fragment) {
+        this.fragment = fragment;
+    }
+
     @Override
     public void onClick(View view) {
         View parent = (View) view.getParent().getParent(); // TextGuess is in the parent parent view
         switch (view.getId()) {
-            case R.id.challenge_one_button_check:
-
-                if (parent != null) {
-                    EditText textGuess = (EditText) parent.findViewById(R.id.challenge_one_text_guess);
-
-                    // Check to see if the user beat the challenge. The code that was here before was
-                    // silly. (And actually for a different type of challenge)
-                    if (textGuess.getText().toString().toLowerCase().equals("poorly-protected-secret")) {
-                        textGuess.setTextColor(parent.getResources().getColor(R.color.color_nebula));
-                        Vibrator vibrator = (Vibrator) parent.getContext().getSystemService(Context.VIBRATOR_SERVICE);
-                        vibrator.vibrate(400);
-                        Toast.makeText(parent.getContext(), "You\'ve completed this challenge!", Toast.LENGTH_LONG).show();
-                    } else {
-                        textGuess.setTextColor(parent.getResources().getColor(R.color.color_nebula_dark));
-                        Toast.makeText(parent.getContext(), "Sorry, that\'s not right..", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                break;
-            case R.id.button_write_file:
-                final Button buttonWrite = (Button) parent.findViewById(R.id.button_write_file);
-
-                // Check to see if the file already exists
-                File file = view.getContext().getFileStreamPath("ANSWER");
-                if (file.exists()) {
-                    file.delete();
-                    buttonWrite.setText(R.string.string_challenge_write_file);
-                    Toast.makeText(parent.getContext(), "File Deleted", Toast.LENGTH_LONG).show();
-
-                } else {
-                    try {
-                        FileOutputStream fileOutputStream = parent.getContext().openFileOutput("ANSWER", Context.MODE_WORLD_READABLE);
-                        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-                        outputStreamWriter.write("poorly-protected-secret");
-                        outputStreamWriter.flush();
-                        outputStreamWriter.close();
-                        Toast.makeText(parent.getContext(), "File Written", Toast.LENGTH_LONG).show();
-                    }
-                    catch (FileNotFoundException e) { e.printStackTrace(); }
-                    catch (IOException e) { e.printStackTrace(); }
-
-                    // Change the text on the button to delete
-                    buttonWrite.setText(R.string.string_challenge_delete_file);
-                }
+            // Check to see whether the phone is in the GPS fenced area
+            case R.id.challenge_four_button_check:
+                fragment.checkLocation();
                 break;
         }
     }
